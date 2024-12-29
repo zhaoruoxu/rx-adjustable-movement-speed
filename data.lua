2f0ffa9ab1249d5d8c83bf7ae3602d830ee2015c
Robert Zhao <729687+zhaoruoxu@users.noreply.github.com>
2024-12-29 18:44:22 +0800

    Add a basic no-op slider.

---
+++ data.lua
@@ -0,0 +1,14 @@
+-- data.lua
+
+data.raw["gui-style"]["default"]["rx_slider"] = {
+    type = "slider_style",
+    align = "center",
+    vertical_align = "center",
+    font = "default-large",
+    draw_notches = true,
+    top_padding = 0,
+    bottom_padding = 0,
+    left_padding = 0,
+    right_padding = 0,
+    width = 200
+}
